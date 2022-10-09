fd123c02690d1e178a6425e483fa5b3ffb6e6475
Sierra Lopez <sierralopez1006@gmail.com>
2022-10-09 10:51:45 -0700

    psuedocode

---
+++ script.js
@@ -0,0 +1,15 @@
+//Add a button that says Play New Game that clears all the data for a new game
+
+//Allow user to enter information in each form. Use event listeners
+
+//Allow user to draw on notepad. use event listeners
+
+//Count the spirals they draw and display that number to them
+
+//That number will be used to count how many answers to skip over before crossing them out and eliminating them from the fortune
+
+//Take user to new page that displays all of their answers in a list dividing out each section the answers are originated
+
+//Stop the elimination of answers in each div after there is only one answer left
+//Once there is only one answer left in each div, stop elimination all together and display fortune
+
