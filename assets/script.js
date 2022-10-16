6f0eb045c655e9b92eeee38111dde3bba159fa6c
Sierra Lopez <sierralopez1006@gmail.com>
2022-10-15 19:54:25 -0700

    fixed form stacking issue

--- assets/script.js
+++ assets/script.js
@@ -8,8 +8,6 @@
 
 //That number will be used to count how many answers to skip over before crossing them out and eliminating them from the fortune
 
-//Take user to new page that displays all of their answers in a list dividing out each section the answers are originated
-
 //Stop the elimination of answers in each div after there is only one answer left
 //Once there is only one answer left in each div, stop elimination all together and display fortune
 
