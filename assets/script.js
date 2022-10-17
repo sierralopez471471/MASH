fb2a3aa30a3240a89f0b1afc0c764021144ed40e
Sierra Lopez <sierralopez1006@gmail.com>
2022-10-17 15:51:55 -0700

    added list.js

--- assets/script.js
+++ assets/script.js
@@ -2,20 +2,19 @@
 
 //Allow user to enter information in each form. Use event listeners
 
+const form = document.getElementById('myform');
 
-let form = document.getElementById('people');
-
-form.addEventListener('submit', callbackFunction);
-
-function callbackFunction(event) {
+form.addEventListener('submit', (event) => {
     event.preventDefault();
     const myFormData = new FormData(event.target);
-    
-    const formDataObj = {};
-    myFormData.forEach((value, key) => (formDataObj[key] = value));
-    console.log(formDataObj);
-};
 
+    const formDataObj = Object.fromEntries(myFormData.entries());
+    console.log(formDataObj);
+});
+
+function addToList() {
+    var formDataObj
+}
 //C:\Users\timot\MyProjects\MASH\assets\script.js
 
 // function myFunction() {
@@ -25,6 +24,7 @@ function callbackFunction(event) {
 //         document.getElementById(fortune[i].id).addEventListener('onclick')
 //     }
 // }
+
 //Allow user to play a mini-game in which they catch hearts, cash, babies, and avoid bombs in an allotted time
 //Hearts, cash, and babies earn player 1 point each but bombs earn -2 points
 //Keep score, and when time runs out display that number to them
@@ -42,11 +42,3 @@ function callbackFunction(event) {
                 <label for="names"> Choose 3 people <span class="required-field">*</span></label>
                 <input type="text" id="names" name="names" required minlength="2" maxlength="35"></input> 
             </div>*/}
-
-// function newImage(url){
-//   let image = document.createElement('img')
-//   image.src = url
-//   image.style.position = 'absolute'
-//   document.body.append(image)
-//   return image
-// }
